e09aeb779e7f26d1a8150a11658044d2b16dbffb
ericeidel <Eric Big Pc>
2013-07-09 20:30:25 -0600

    Changed to right value

--- Game3/src/abilities/Damage.java
+++ Game3/src/abilities/Damage.java
@@ -2,7 +2,7 @@ package abilities;
 
 public class Damage 
 {
-	public static final int EARTH = 21;
+	public static final int EARTH = 1;
 	public static final int AIR = 2;
 	public static final int WATER = 3;
 	public static final int FIRE = 4;
